af4b414ceec36d08c13b0c3c19b0acfc6b1fc5c9
Yakov Algaev <yakov.algaev@ispring.com>
2021-11-26 17:10:39 +0300

    Увеличил поля navbar

--- components/navigation/NavigationPanel.tsx
+++ components/navigation/NavigationPanel.tsx
@@ -3,7 +3,7 @@ import {LogoIcon} from '../icons/LogoIcon'
 import {AuthButtons} from './AuthButtons'
 
 function NavigationPanel() {
-    return <nav className="header sticky top-0 bg-white shadow-md flex items-center justify-between py-2 px-8">
+    return <nav className="header sticky top-0 bg-white shadow-md flex items-center justify-between py-3 px-8">
 		<Link href='/main-space'>
 			<a className="flex align-middle">
 				<LogoIcon size={36} />
