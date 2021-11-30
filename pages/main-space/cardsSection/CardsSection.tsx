1097ba43c794154529eb06fe5e35a00530cb4fac
Yakov Algaev <yakov.algaev@ispring.com>
2021-11-30 15:59:51 +0300

    Добавил компонент карточки

--- pages/main-space/cardsSection/CardsSection.tsx
+++ pages/main-space/cardsSection/CardsSection.tsx
@@ -1,19 +1,25 @@
+import {BankCard} from '../../../components/bankCard/BankCard'
 import {DatePicker} from '../../../components/datePicker/DatePicker'
 import {CalendarIcon} from '@heroicons/react/outline'
 import {useState} from 'react'
 
 function CardsSection() {
 	const [selectedDate, setSelectedDate] = useState(new Date())
+	//TODO
+	const cards = ['1', '2', '3', '4', '5', '6']
 
 	return (
-		<div className='flex justify-center w-3/12 bg-green-100'>
+		<div className='flex flex-col items-center w-3/12 bg-green-100'>
 			<DatePicker
 				date={selectedDate}
 				onSelectedChanged={setSelectedDate}
-				className='m-2 w-36'
+				className='my-4 w-36'
 				inputStyle='string'
 				icon={<CalendarIcon className='w-5 h-5 text-gray-600' />}
 			/>
+			<div className='flex flex-col space-y-4'>
+				{cards.map(card => <BankCard key={card} />)}
+			</div>
 		</div>)
 }
 
