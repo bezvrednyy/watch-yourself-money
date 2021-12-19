980a4f4a74fe414a9582c2be7d1a40684f4601a2
Yakov Algaev <algaev18@gmail.com>
2021-12-19 11:42:58 +0300

    Вынес контент в отдельный файл

--- pages/main-space/categoriesSection/view/editCategoryPopup/EditCategoryPopup.tsx
+++ pages/main-space/categoriesSection/view/editCategoryPopup/EditCategoryPopup.tsx
@@ -1,10 +1,10 @@
 import {ExternalLayer} from '../../../../../components/layers/ExternalLayer'
-import {TextField} from '../../../../../components/TextField'
 import {useAction, useAtom} from '@reatom/react'
 import {editCategoryPopupAtoms} from './model/editableCategoryAtom'
 import {categoriesAtom, editableCategoryIdAtom} from '../../model/categoriesAtom'
 import {verify} from '../../../../../common/verify'
 import {useEffect} from 'react'
+import {EditCategoryPopupContent} from './EditCategoryPopupContent'
 
 type EditCategoryPopupProps = {
 	show: boolean,
@@ -12,13 +12,13 @@ type EditCategoryPopupProps = {
 	onSave: () => void,
 }
 
-function EditCategoryPopup(props: EditCategoryPopupProps) {
+export function EditCategoryPopup(props: EditCategoryPopupProps) {
 	useInitPopupAtoms()
 
 	return <ExternalLayer
 		show={props.show}
 		onClose={props.onClose}
-		createJSX={() => <EditCategoryPopupContent {...props} />}
+		createJSX={() => <EditCategoryPopupContent />}
 	/>
 }
 
@@ -47,44 +47,4 @@ function useInitPopupAtoms() {
 		handleSetSubcategories,
 		handleSetTitle,
 	])
-}
-
-
-function EditCategoryPopupContent({
-	onClose,
-	onSave,
-}: EditCategoryPopupProps) {
-	const [title] = useAtom(editCategoryPopupAtoms.titleAtom)
-	const handleSetTitle = useAction(editCategoryPopupAtoms.titleAtom.set)
-
-	return (
-		<div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl'>
-			<TextField
-				value={title}
-				onInput={handleSetTitle}
-				placeholder={'Category name'}
-				required={true}
-			/>
-			<div className='mt-2'>
-				<p className='text-sm text-gray-500'>
-					Your payment has been successfully submitted. We’ve sent you
-					an email with all of the details of your order.
-				</p>
-			</div>
-
-			<div className='mt-4'>
-				<button
-					type='button'
-					className='inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500'
-					onClick={onClose}
-				>
-					Got it, thanks!
-				</button>
-			</div>
-		</div>
-	)
-}
-
-export {
-	EditCategoryPopup,
 }
---
+++ pages/main-space/categoriesSection/view/editCategoryPopup/EditCategoryPopupContent.tsx
@@ -0,0 +1,35 @@
+import {useAction, useAtom} from '@reatom/react'
+import {editCategoryPopupAtoms} from './model/editableCategoryAtom'
+import {TextField} from '../../../../../components/TextField'
+
+function EditCategoryPopupContent() {
+	const [title] = useAtom(editCategoryPopupAtoms.titleAtom)
+	const handleSetTitle = useAction(editCategoryPopupAtoms.titleAtom.set)
+
+	return (
+		<div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl'>
+			<TextField
+				value={title}
+				onInput={handleSetTitle}
+				placeholder={'Category name'}
+				required={true}
+			/>
+
+			<div className='mt-4'>
+				<button
+					type='button'
+					className='inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500'
+					onClick={() => {
+						//TODO
+					}}
+				>
+					Save
+				</button>
+			</div>
+		</div>
+	)
+}
+
+export {
+	EditCategoryPopupContent,
+}
