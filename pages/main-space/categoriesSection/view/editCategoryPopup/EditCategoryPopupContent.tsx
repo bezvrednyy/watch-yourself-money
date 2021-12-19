9c74577aa6528f7d27a3cd47ab7167ec69e8d383
Yakov Algaev <algaev18@gmail.com>
2021-12-19 17:39:43 +0300

    Внедрил popper, реализовал первоначальную версию ButtonWithPopover

--- pages/main-space/categoriesSection/view/editCategoryPopup/EditCategoryPopupContent.tsx
+++ pages/main-space/categoriesSection/view/editCategoryPopup/EditCategoryPopupContent.tsx
@@ -1,6 +1,7 @@
 import {useAction, useAtom} from '@reatom/react'
 import {editCategoryPopupAtoms} from './model/editableCategoryAtom'
 import {TextField} from '../../../../../components/TextField'
+import {ViewPicker} from './view/ViewPicker'
 
 function EditCategoryPopupContent() {
 	const [title] = useAtom(editCategoryPopupAtoms.titleAtom)
@@ -8,6 +9,7 @@ function EditCategoryPopupContent() {
 
 	return (
 		<div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl'>
+			<ViewPicker/>
 			<TextField
 				value={title}
 				onInput={handleSetTitle}
---
+++ pages/main-space/categoriesSection/view/editCategoryPopup/view/ViewPicker.tsx
@@ -0,0 +1,46 @@
+import {Popover, Transition} from '@headlessui/react'
+import {Fragment, useState} from 'react'
+import {usePopper} from 'react-popper'
+
+//TODO: ButtonWithPopover
+export function ViewPicker() {
+	const [referenceElement, setReferenceElement] = useState<HTMLButtonElement|null>(null)
+	const [popperElement, setPopperElement] = useState<HTMLDivElement|null>()
+	const {styles, attributes} = usePopper(referenceElement, popperElement)
+
+	//Panel всегда должна быть в DOM, т. к. за ней следит popper. Поэтому Transition закинут внутрь: https://popper.js.org/docs/v2/modifiers/hide/
+	return (
+		<Popover>
+			{({open}) => (
+				<>
+					<Popover.Button
+						ref={setReferenceElement}
+					>
+						Solutions
+					</Popover.Button>
+
+					<Popover.Panel
+						className={`z-10`}
+						ref={setPopperElement}
+						style={styles.popper}
+						{...attributes.popper}
+					>
+						<Transition
+							as={Fragment}
+							enter='transition ease-out duration-200'
+							enterFrom='opacity-0 translate-y-1'
+							enterTo='opacity-100 translate-y-0'
+							leave='transition ease-in duration-150'
+							leaveFrom='opacity-100 translate-y-0'
+							leaveTo='opacity-0 translate-y-1'
+						>
+							<div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white p-7'>
+
+							</div>
+						</Transition>
+					</Popover.Panel>
+				</>
+			)}
+		</Popover>
+	)
+}
