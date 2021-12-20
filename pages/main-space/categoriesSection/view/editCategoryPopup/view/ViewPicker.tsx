6356a20b261181048ec2029294f5caa484116762
Yakov Algaev <algaev18@gmail.com>
2021-12-20 11:43:09 +0300

    Добавил портал для корректного отображения ButtonWithPopover

--- pages/main-space/categoriesSection/view/editCategoryPopup/view/ViewPicker.tsx
+++ pages/main-space/categoriesSection/view/editCategoryPopup/view/ViewPicker.tsx
@@ -1,6 +1,7 @@
 import {Popover, Transition} from '@headlessui/react'
 import {Fragment, useState} from 'react'
 import {usePopper} from 'react-popper'
+import {Portal} from 'next/dist/client/portal'
 
 //TODO: ButtonWithPopover
 export function ViewPicker() {
@@ -18,27 +19,28 @@ export function ViewPicker() {
 					>
 						Solutions
 					</Popover.Button>
-
-					<Popover.Panel
-						className={`z-10`}
-						ref={setPopperElement}
-						style={styles.popper}
-						{...attributes.popper}
-					>
-						<Transition
-							as={Fragment}
-							enter='transition ease-out duration-200'
-							enterFrom='opacity-0 translate-y-1'
-							enterTo='opacity-100 translate-y-0'
-							leave='transition ease-in duration-150'
-							leaveFrom='opacity-100 translate-y-0'
-							leaveTo='opacity-0 translate-y-1'
+					<Portal type='popover-layer'>
+						<Popover.Panel
+							className='z-10'
+							ref={setPopperElement}
+							style={styles.popper}
+							{...attributes.popper}
 						>
-							<div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white p-7'>
-
-							</div>
-						</Transition>
-					</Popover.Panel>
+							<Transition
+								as={Fragment}
+								enter='transition ease-out duration-200'
+								enterFrom='opacity-0 translate-y-1'
+								enterTo='opacity-100 translate-y-0'
+								leave='transition ease-in duration-150'
+								leaveFrom='opacity-100 translate-y-0'
+								leaveTo='opacity-0 translate-y-1'
+							>
+								<div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white p-7'>
+									<div>HelloHelloHello!</div>
+								</div>
+							</Transition>
+						</Popover.Panel>
+					</Portal>
 				</>
 			)}
 		</Popover>
