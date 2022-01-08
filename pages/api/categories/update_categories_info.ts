7f686b7ff42768e282c935513421b2c1ef9b6dd5
yakov.algaev <bezvrednyy@gmail.com>
2022-01-08 11:23:55 +0300

    Подготовил первоначальную версию роута обновления категории

--- pages/api/categories/update_categories_info.ts
+++ pages/api/categories/update_categories_info.ts
@@ -1,5 +1,8 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import {getSession} from 'next-auth/react'
 import {OutlineIconId} from '../../../components/icons/getOutlineIconById'
 import {ColorId} from '../../../common/colors/colors'
+import prisma from '../../../prisma/prisma'
 import {CategoryData} from '../../main-space/categoriesSection/model/categoriesAtom'
 
 export type UpdateCategoriesInfoRequest = {
@@ -11,4 +14,34 @@ export type UpdateCategoriesInfoRequest = {
 	newSubcategories: Array<CategoryData>,
 	removedSubcategoryIds: Array<number>,
 	haveBecomeMainCategoriesIds: Array<number>,
+}
+
+type UpdateCategoriesApiRequest = NextApiRequest & {
+	body: {
+		data: UpdateCategoriesInfoRequest,
+	}
+}
+
+export default async function updateCategories(req: UpdateCategoriesApiRequest, res: NextApiResponse) {
+	const session = await getSession({ req })
+	if (!session?.user) {
+		res.status(401).redirect('/api/auth/signin')
+	}
+	const {removedSubcategoryIds} = req.body.data
+
+	try {
+		prisma.category.deleteMany({
+			where: {
+				id: {
+					in: removedSubcategoryIds,
+				},
+			},
+		})
+		res.status(200).send({})
+	}
+	catch (error) {
+		res.status(500).json({
+			error,
+		})
+	}
 }
