37242c3252dfae76c9bbefab427c6e197973df63
yakov.algaev <bezvrednyy@gmail.com>
2022-01-08 17:49:02 +0300

    Добавил первоначальную версию Disclosure

---
+++ components/DisclosureDefault.tsx
@@ -0,0 +1,31 @@
+import {Disclosure} from '@headlessui/react'
+import {Fragment} from 'react'
+
+type DisclosureDefaultProps = {
+	createButton: (open: boolean) => JSX.Element,
+	createPanel: () => JSX.Element
+}
+
+export default function DisclosureDefault({
+	createButton,
+	createPanel,
+}: DisclosureDefaultProps) {
+	return (
+		<div className='w-full px-4 pt-16'>
+			<div className='w-full max-w-md p-2 mx-auto bg-white rounded-2xl'>
+				<Disclosure>
+					{({open}) => (
+						<>
+							<Disclosure.Panel as={Fragment}>
+								{createPanel()}
+							</Disclosure.Panel>
+							<Disclosure.Button as={Fragment}>
+								{createButton(open)}
+							</Disclosure.Button>
+						</>
+					)}
+				</Disclosure>
+			</div>
+		</div>
+	)
+}
--- pages/main-space/historySection/HistorySection.tsx
+++ pages/main-space/historySection/HistorySection.tsx
@@ -1,4 +1,7 @@
+import {MinusIcon, PlusIcon} from '@heroicons/react/solid'
 import {getMilliseconds} from 'date-fns'
+import {joinClassNames} from '../../../common/joinClassNames'
+import DisclosureDefault from '../../../components/DisclosureDefault'
 import {ViewTransactionInfo} from './content/TransactionHistorySectionItem'
 import {DayTransactionsHistorySection} from './content/DayTransactionsHistorySection'
 
@@ -9,6 +12,7 @@ type DayTransitionsData = {
 
 function HistorySection() {
 	const transactionsByDays: Array<DayTransitionsData> = []
+	const iconClass = 'w-5 h-5 text-purple-500'
 	return (
 		<div className='w-4/12 bg-white py-5'>
 			{transactionsByDays.map(x => <DayTransactionsHistorySection
@@ -16,6 +20,16 @@ function HistorySection() {
 				dayDate={x.dayDate}
 				transitions={x.transitions}
 			/>)}
+			<DisclosureDefault
+				createButton={open => <div className={joinClassNames(
+					'flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg',
+					'hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75',
+				)}>
+					<span>Add new transaction</span>
+					{open ? <MinusIcon className={iconClass} /> : <PlusIcon className={iconClass} />}
+				</div>}
+				createPanel={() => <div>Hello</div>}
+			/>
 		</div>)
 }
 
