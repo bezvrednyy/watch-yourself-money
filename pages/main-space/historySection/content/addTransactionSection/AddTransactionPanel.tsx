19a80c6f4f776b0b20d9097427ca92634d06181d
yakov.algaev <bezvrednyy@gmail.com>
2022-01-09 12:04:13 +0300

    Перевёл панельку на атомы, добавил возможность выбора категории

--- pages/main-space/historySection/content/addTransactionSection/AddTransactionPanel.tsx
+++ pages/main-space/historySection/content/addTransactionSection/AddTransactionPanel.tsx
@@ -1,22 +1,31 @@
-import {useAtom} from '@reatom/react'
-import {useState} from 'react'
+import {useAction, useAtom} from '@reatom/react'
+import {getColorById} from '../../../../../common/colors/theme'
 import {joinClassNames} from '../../../../../common/joinClassNames'
+import {verify} from '../../../../../common/verify'
+import {ButtonWithPopover} from '../../../../../components/button/buttons/buttonWithPopover/ButtonWithPopover'
+import {getOutlineIconById} from '../../../../../components/icons/getOutlineIconById'
+import {PopoverDefault} from '../../../../../components/popovers/PopoverDefault'
+import {RoundedSquare} from '../../../../../components/RoundedSquare'
 import {TextField} from '../../../../../components/textField/TextField'
 import {getCurrencySymbolById, userSettingsAtom} from '../../../../../environment/userSettingsAtom'
+import {categoriesAtom} from '../../../model/categoriesAtom'
 import styles from './AddTransactionPanel.module.css'
+import {addTransactionSectionAtoms} from './model/addTransactionSectionAtoms'
 
 export function AddTransactionPanel() {
-	const [sum, setSum] = useState('')
 	const [userSettings] = useAtom(userSettingsAtom)
+	const [sum] = useAtom(addTransactionSectionAtoms.sumAtom)
+	const handleSetSum = useAction(addTransactionSectionAtoms.sumAtom.set)
+
 	const currencySymbol = getCurrencySymbolById(userSettings.currencyId)
 
 	return <div>
 		<div className='flex items-center'>
-			<div>Category</div>
+			<CategoryPicker/>
 			<TextField
 				style='link'
-				value={sum}
-				onInput={value => setSum(value)}
+				value={`${sum}`}
+				onInput={value => handleSetSum(Number(value))}
 				size='xLarge'
 				placeholder='100'
 				required={true}
@@ -29,4 +38,43 @@ export function AddTransactionPanel() {
 			/>
 		</div>
 	</div>
+}
+
+function CategoryPicker() {
+	const [selectedCategoryId] = useAtom(addTransactionSectionAtoms.selectedCategoryIdAtom)
+	const [categories] = useAtom(categoriesAtom)
+	const selectedCategory = verify(categories.mainCategories.find(x => x.id === selectedCategoryId))
+	const IconFC = getOutlineIconById(selectedCategory.iconId)
+
+	return <ButtonWithPopover
+		createButton={() => <RoundedSquare
+			createIcon={() => <IconFC className='m-1 w-6 h-6 overflow-hidden' />}
+			bgHexColor={getColorById(selectedCategory.colorId)}
+			className='transform transition hover:scale-105 cursor-pointer shadow'
+		/>}
+		createPopover={() => <PopoverDefault
+			createContent={() => <PopoverContent/>}
+		/>}
+	/>
+}
+
+function PopoverContent() {
+	const [categories] = useAtom(categoriesAtom)
+	const handleSetSelectCategoryId = useAction(addTransactionSectionAtoms.selectedCategoryIdAtom.set)
+
+	return (
+		<div className='flex flex-wrap'>
+			{categories.mainCategories.map(item => {
+				const Icon = getOutlineIconById(item.iconId)
+				return <RoundedSquare
+					key={item.id}
+					createIcon={() => <Icon className='m-1 w-6 h-6 overflow-hidden'/>}
+					title={item.title}
+					onClick={() => handleSetSelectCategoryId(item.id)}
+					bgHexColor={getColorById(item.colorId)}
+					className='m-1 opacity-90 transform transition hover:scale-105 cursor-pointer hover:opacity-100 shadow'
+				/>
+			})}
+		</div>
+	)
 }
