25dcd424338144f4d1ebe97f1b43bd678870c95b
yakov.algaev <bezvrednyy@gmail.com>
2022-01-09 16:37:02 +0300

    Добавил EndDatePicker

--- pages/main-space/cardsSection/CardsSection.tsx
+++ pages/main-space/cardsSection/CardsSection.tsx
@@ -5,22 +5,35 @@ import styles from './CardsSection.module.css'
 import {joinClassNames} from '../../../common/joinClassNames'
 import {BankCard} from '../../../components/bankCard/BankCard'
 import {DatePicker} from '../../../components/datePicker/DatePicker'
-import {CalendarIcon} from '@heroicons/react/outline'
 
 function CardsSection() {
 	const [selectedPeriod] = useAtom(selectedPeriodAtom)
 	const [cards] = useAtom(bankAccountsAtom)
 	const handleSetStartDate = useAction(selectedPeriodAtom.setStartDate)
+	const handleSetEndDate = useAction(selectedPeriodAtom.setEndDate)
 
 	return (
 		<div className='flex flex-col w-96 bg-green-100'>
-			<DatePicker
-				date={selectedPeriod.startDate}
-				onSelectedChanged={handleSetStartDate}
-				className='my-4 w-36 self-center'
-				inputStyle='string'
-				icon={<CalendarIcon className='w-5 h-5 text-gray-600' />}
-			/>
+			<div className='flex justify-center'>
+				<DatePicker
+					date={selectedPeriod.startDate}
+					onSelectedChanged={handleSetStartDate}
+					className={joinClassNames(
+						'my-4',
+						styles['date-picker'],
+					)}
+					inputStyle='string'
+				/>
+				<DatePicker
+					date={selectedPeriod.endDate}
+					onSelectedChanged={handleSetEndDate}
+					className={joinClassNames(
+						'my-4',
+						styles['date-picker'],
+					)}
+					inputStyle='string'
+				/>
+			</div>
 			<div className={joinClassNames('flex flex-col space-y-4 overflow-auto items-center', styles['cards-section'])}>
 				{cards.map(card => <BankCard key={card.id} {...card} />)}
 			</div>
