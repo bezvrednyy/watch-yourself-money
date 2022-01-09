262b854f9dbf27446703dc853307bca9d6dbaab8
yakov.algaev <bezvrednyy@gmail.com>
2022-01-09 13:13:41 +0300

    Добавил обнуление selectedSubcategoryIdAtom при смене основной категории

--- pages/main-space/historySection/content/addTransactionSection/model/addTransactionSectionAtoms.ts
+++ pages/main-space/historySection/content/addTransactionSection/model/addTransactionSectionAtoms.ts
@@ -1,8 +1,19 @@
+import {createAtom} from '@reatom/core'
 import {createPrimitiveAtom} from '@reatom/core/primitives'
 
 const selectedCategoryIdAtom = createPrimitiveAtom<string>('')
 const sumAtom = createPrimitiveAtom<number>(0)
-const selectedSubcategoryIdAtom = createPrimitiveAtom<string|null>(null)
+const selectedSubcategoryIdAtom = createAtom(
+	{
+		set: (v: string|null) => v,
+		selectedCategoryIdAtom,
+	},
+	(track, state: string|null = null) => {
+		track.onAction('set', v => (state = v))
+		track.onChange('selectedCategoryIdAtom', () => (state = null))
+		return state
+	},
+)
 
 export const addTransactionSectionAtoms = {
 	selectedCategoryIdAtom,
