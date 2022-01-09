10111b9caef065a8a4bdb656a626cfe4560ec5bc
yakov.algaev <bezvrednyy@gmail.com>
2022-01-09 17:58:43 +0300

    Реализовал MenuDefault

---
+++ components/MenuDefault.tsx
@@ -0,0 +1,46 @@
+import {Menu} from '@headlessui/react'
+import {useState} from 'react'
+import {usePopper} from 'react-popper'
+import {joinClassNames} from '../common/joinClassNames'
+
+type MenuDefaultProps = {
+	createButton: () => JSX.Element,
+	items: Array<JSX.Element>, //Menu.Item components
+	popoverClass?: string,
+}
+
+export function MenuDefault({
+	createButton,
+	items,
+	popoverClass,
+}: MenuDefaultProps) {
+	const [referenceElement, setReferenceElement] = useState<HTMLButtonElement|null>(null)
+	const [popperElement, setPopperElement] = useState<HTMLDivElement|null>()
+	const {styles, attributes} = usePopper(referenceElement, popperElement, {
+		modifiers: [{
+			name: 'offset',
+			options: {
+				offset: [0, 5],
+			},
+		}],
+	})
+
+	return (
+		<Menu as='div' className='relative inline-block text-left'>
+			<Menu.Button ref={setReferenceElement}>
+				{createButton()}
+			</Menu.Button>
+			<Menu.Items
+				className={joinClassNames(
+					'rounded-md shadow-lg',
+					popoverClass,
+				)}
+				ref={setPopperElement}
+				style={styles.popper}
+				{...attributes.popper}
+			>
+				{items}
+			</Menu.Items>
+		</Menu>
+	)
+}
