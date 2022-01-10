7274373313a1741cfd6f40048c15b7733429fd96
yakov.algaev <bezvrednyy@gmail.com>
2022-01-10 19:26:55 +0300

    Завёл помощников joinTexts и fetchPostData

---
+++ common/clientApi/fetchPostData.ts
@@ -0,0 +1,12 @@
+export async function fetchPostData<DATA>(url: string, data: DATA): Promise<Response> {
+	const result = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+		},
+		body: JSON.stringify({
+			data,
+		}),
+	})
+	return result
+}
--- common/joinClassNames.ts
+++ common/joinClassNames.ts
@@ -2,6 +2,11 @@ function joinClassNames(...classes: (string|undefined|null)[]): string {
 	return classes.filter(Boolean).join(' ')
 }
 
+function joinTexts(...texts: (string|undefined|null)[]): string {
+	return texts.filter(Boolean).join(' ')
+}
+
 export {
 	joinClassNames,
+	joinTexts,
 }
