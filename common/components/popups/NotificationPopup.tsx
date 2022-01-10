c773afd9c85e7263a0509470a166ec6b50c8ef49
yakov.algaev <bezvrednyy@gmail.com>
2022-01-10 19:25:58 +0300

    Реализовал NotificationPopup

---
+++ common/components/popups/NotificationPopup.tsx
@@ -0,0 +1,47 @@
+import {Button} from '../../../uikit/button/Button'
+import {PopupDefault} from '../../../uikit/PopupDefault'
+
+type NotificationPopupProps = {
+	show: boolean,
+	description: string,
+	onAccept?: () => void,
+	onCancel?: () => void,
+	additionalButtons?: Array<JSX.Element>,
+}
+
+export function NotificationPopup({
+	show,
+	description,
+	onAccept,
+	onCancel,
+	additionalButtons,
+}: NotificationPopupProps) {
+	const buttons = []
+	!!onAccept && buttons.push(<Button
+		key='ok'
+		style='blue-default'
+		onClick={onAccept}
+		structure='text'
+		text='Ok'
+	/>)
+	!!additionalButtons && buttons.push(...additionalButtons)
+	!!onCancel && buttons.push(<Button
+		key='cancel'
+		style='secondary'
+		onClick={onCancel}
+		structure='text'
+		text='Cancel'
+	/>)
+
+	return <PopupDefault
+		createContent={() => (
+			<div className=''>
+				<p className=''>
+					{description}
+				</p>
+			</div>
+		)}
+		buttons={buttons}
+		show={show}
+	/>
+}
