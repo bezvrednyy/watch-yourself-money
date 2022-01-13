2497ffa05806dd05909ac6f8e03bd7386fe65554
yakov.algaev <bezvrednyy@gmail.com>
2022-01-13 18:36:21 +0300

    Завёл создателей BackendEitherObject, вместо конструирования полноценного Either

--- backFrontJoint/backendApi/sendJsonData.ts
+++ backFrontJoint/backendApi/sendJsonData.ts
@@ -1,10 +1,10 @@
-import {left, right} from '@sweet-monads/either'
 import {NextApiResponse} from 'next'
+import {leftBackendEitherObject, rightBackendEitherObject} from '../common/contracts/BackendEitherObject'
 
 export function sendJsonRightData<T>(res: NextApiResponse, data: T) {
-	res.status(200).json(right(data))
+	res.status(200).json(rightBackendEitherObject(data))
 }
 
 export function sendJsonLeftData<T>(res: NextApiResponse, code: number, data: T) {
-	res.status(code).json(left(data))
+	res.status(code).json(leftBackendEitherObject(data))
 }
--- backFrontJoint/common/contracts/BackendEitherObject.ts
+++ backFrontJoint/common/contracts/BackendEitherObject.ts
@@ -1,15 +1,27 @@
 import {Either, left, right} from '@sweet-monads/either'
 
-export type BackendEitherObject<L, R> = {
+type LeftEitherObject<L> = {
 	type: 'Left',
 	value: L
-} | {
+}
+
+type RightEitherObject<R> = {
 	type: 'Right',
 	value: R,
 }
+
+export type BackendEitherObject<L, R> = LeftEitherObject<L> | RightEitherObject<R>
 
 export function processBackendEither<L, R>(backendEither: BackendEitherObject<L, R>): Either<L, R> {
 	return backendEither.type === 'Left'
 		? left(backendEither.value)
 		: right(backendEither.value)
+}
+
+export function leftBackendEitherObject<T>(value: T): LeftEitherObject<T> {
+	return { type: 'Left', value }
+}
+
+export function rightBackendEitherObject<T>(value: T): RightEitherObject<T> {
+	return { type: 'Right', value }
 }
