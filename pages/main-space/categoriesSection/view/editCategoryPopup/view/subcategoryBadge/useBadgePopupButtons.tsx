335ebc7901204382cec0f0187a54b4782891edf5
yakov.algaev <bezvrednyy@gmail.com>
2022-01-14 17:21:21 +0300

    Добавил кнопку отмены + косметические правки.

--- pages/main-space/categoriesSection/view/editCategoryPopup/view/subcategoryBadge/useBadgePopupButtons.tsx
+++ pages/main-space/categoriesSection/view/editCategoryPopup/view/subcategoryBadge/useBadgePopupButtons.tsx
@@ -22,70 +22,39 @@ export function useBadgePopupButtons({
 	const handleTurnInMainSubcategory = useAction(editCategoryPopupAtoms.subcategoriesAtom.turnInMain)
 	const type = useSubcategoryType(props.id)
 
-	const updateFn = () => handleUpdateSubcategory({
-		...props,
-		iconId: newIconId,
-		title: newTitle,
-	})
+	const updateFn = () => {
+		handleUpdateSubcategory({
+			...props,
+			iconId: newIconId,
+			title: newTitle,
+		})
+		setShow(false)
+	}
+	const removeFn = () => {
+		handleRemoveSubcategory(props.id)
+		setShow(false)
+	}
+	const turnInMainFn = () => {
+		handleTurnInMainSubcategory(props.id)
+		setShow(false)
+	}
 
 	const buttons: Array<JSX.Element> = []
 
 	if (type === 'removed') {
-		buttons.push(<Button
-			key='restore'
-			style='blue-default'
-			onClick={() => {
-				updateFn()
-				setShow(false)
-			}}
-			structure='text'
-			text='Restore'
-		/>)
+		buttons.push(<Button key='restore' style='blue-default' onClick={updateFn} structure='text' text='Restore' />)
 	}
 	else if (type === 'turnInMain') {
-		buttons.push(<Button
-			key='turnInSub'
-			style='blue-default'
-			onClick={() => {
-				updateFn()
-				setShow(false)
-			}}
-			structure='text'
-			text='Turn in sub'
-		/>)
+		buttons.push(<Button key='turnInSub' style='blue-default' onClick={updateFn} structure='text' text='Turn in sub' />)
 	}
 	else {
-		buttons.push(<Button
-			key='save'
-			style='blue-default'
-			onClick={() => {
-				updateFn()
-				setShow(false)
-			}}
-			structure='text'
-			text='Save'
-		/>)
-		buttons.push(<Button
-			key='remove'
-			style='destructure'
-			onClick={() => {
-				handleRemoveSubcategory(props.id)
-				setShow(false)
-			}}
-			structure='text'
-			text='Remove'
-		/>)
-		buttons.push(<Button
-			key='turnInMain'
-			style='secondary'
-			onClick={() => {
-				handleTurnInMainSubcategory(props.id)
-				setShow(false)
-			}}
-			structure='text'
-			text='Turn in main'
-		/>)
+		buttons.push(<Button key='save' style='blue-default' onClick={updateFn} structure='text' text='Save' />)
+		buttons.push(<Button key='remove' style='destructure' onClick={removeFn} structure='text' text='Remove' />)
+		if (type === 'default') {
+			buttons.push(<Button key='turnInMain' style='secondary' onClick={turnInMainFn} structure='text' text='Turn in main' />)
+		}
 	}
 
+	buttons.push(<Button key='close' style='secondary' onClick={() => setShow(false)} structure='text' text='Cancel'/>)
 	return buttons
 }
