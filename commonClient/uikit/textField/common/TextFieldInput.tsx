0260677ef7b249ffbfe072d1ae9e32b9cb9af2da
yakov.algaev <bezvrednyy@gmail.com>
2022-01-19 15:18:37 +0300

    Добавил хук useEventHandler. Добавил срабатывание onBlur в TextFieldInput при нажатии на Enter

--- commonClient/uikit/textField/common/TextFieldInput.tsx
+++ commonClient/uikit/textField/common/TextFieldInput.tsx
@@ -1,3 +1,5 @@
+import {useRef} from 'react'
+import {useEventHandler} from '../../../hooks/useEventHandler'
 import {TextFieldInputProps} from '../textFieldProps'
 
 export function TextFieldInput({
@@ -11,8 +13,17 @@ export function TextFieldInput({
 	onFocus,
 	onBlur,
 }: TextFieldInputProps) {
+	const ref = useRef<HTMLInputElement>(null)
+	useEventHandler(ref, 'keydown', event => {
+		if (event.code === 'Enter') {
+			ref.current && ref.current.blur()
+			onBlur && onBlur()
+		}
+	})
+
 	return (
 		<input
+			ref={ref}
 			value={value}
 			onInput={event => onInput(event.currentTarget.value)}
 			onFocus={onFocus}
