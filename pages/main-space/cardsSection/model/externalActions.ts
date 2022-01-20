5abe6014e5d1eb7bfa0cd5d2aace94ac1ae61f62
yakov.algaev <bezvrednyy@gmail.com>
2022-01-20 16:39:37 +0300

    Реализовал валидацию данных при добавлении нового счёта

--- pages/main-space/cardsSection/model/externalActions.ts
+++ pages/main-space/cardsSection/model/externalActions.ts
@@ -1,18 +1,37 @@
 import {getClientApi, processStandardError} from '../../../../backFrontJoint/clientApi/clientApi'
+import {isNumber} from '../../../../common/utils/number'
+import {removeSpaces, trimAll} from '../../../../common/utils/string'
 import {declareAloneAction} from '../../../../commonClient/declareAloneAction'
 import {updateBankAccountsAction} from '../../model/bankAccountsAtom'
 import {addAccountButtonAtoms} from './addAccountButtonAtoms'
 
-const createBankCard = declareAloneAction(async store => {
-	const {statusesAtom, nameAtom, balanceAtom} = addAccountButtonAtoms
-	store.dispatch(statusesAtom.setSaving())
-	//TODO:cash Провалидировать, убрать от лишних пробелов и сохранить данные.
-	const name = store.getState(nameAtom)
-	const money = Number(store.getState(balanceAtom))
+type CreateBankCardPayload = {
+	name: string,
+	balance: string,
+}
 
+const createBankCard = declareAloneAction(async (store, {
+	name,
+	balance,
+}: CreateBankCardPayload) => {
+	const {statusesAtom, errorsSetAtom} = addAccountButtonAtoms
+	let hasError = false
+	const stringNumber = removeSpaces(balance)
+	const preparedName = trimAll(name)
+	if (!isNumber(stringNumber)) {
+		store.dispatch(errorsSetAtom.addError('invalidBalance'))
+		hasError = true
+	}
+	if (!preparedName) {
+		store.dispatch(errorsSetAtom.addError('invalidName'))
+		hasError = true
+	}
+	if (hasError) return
+
+	store.dispatch(statusesAtom.setSaving())
 	const either = await getClientApi().bankAccounts.createBankAccount({
-		name,
-		money,
+		name: preparedName,
+		money: Number(balance),
 	})
 	either
 		.mapRight(async () => {
--- pages/main-space/cardsSection/view/AddAccountButton.tsx
+++ pages/main-space/cardsSection/view/AddAccountButton.tsx
@@ -1,3 +1,4 @@
+import styles from './AddAccountButton.module.css'
 import {isNumber} from '../../../../common/utils/number'
 import {formatMoney} from '../../../../common/utils/productUtils'
 import {joinStrings, removeSpaces} from '../../../../common/utils/string'
@@ -12,6 +13,7 @@ import {cardsSectionExternalActions} from '../model/externalActions'
 
 function AddAccountButton() {
 	const [status] = useAtom(addAccountButtonAtoms.statusesAtom)
+	const [errorsSet] = useAtom(addAccountButtonAtoms.errorsSetAtom)
 	const handleSetOpened = useAction(addAccountButtonAtoms.statusesAtom.setOpened)
 
 	function getContent(): JSX.Element {
@@ -25,12 +27,24 @@ function AddAccountButton() {
 		return <AddAccountContent/>
 	}
 
+	function getWrapperStyles() {
+		console.log('update')
+		const hasError = [...errorsSet].length
+		if (hasError) {
+			return `shadow-lg shadow-red-200 bg-white ${styles['card-error']}`
+		}
+		return joinStrings(
+			status === 'normal' ? 'bg-slate-50 shadow-lg' : 'bg-white shadow-xl',
+			'hover:shadow-xl transition duration-300',
+		)
+	}
+
 	return (
 		<div
 			className={joinStrings(
 				'group flex box-border w-full px-8 h-20 rounded-full items-center justify-center',
-				'cursor-pointer hover:bg-white hover:shadow-xl transition duration-300',
-				status === 'normal' ? 'bg-slate-50 shadow-lg' : 'bg-white shadow-xl'
+				'cursor-pointer hover:bg-white',
+				getWrapperStyles()
 			)}
 			onClick={handleSetOpened}
 		>
@@ -42,8 +56,10 @@ function AddAccountButton() {
 function AddAccountContent() {
 	const [name, setName] = useState('')
 	const [balance, setBalance] = useState('')
+	const [errorsSet] = useAtom(addAccountButtonAtoms.errorsSetAtom)
 	const handleSetNormal = useAction(addAccountButtonAtoms.statusesAtom.setNormal)
-
+	const handleClearErrors = useAction(addAccountButtonAtoms.errorsSetAtom.clear)
+	const handleRemoveError = useAction(addAccountButtonAtoms.errorsSetAtom.removeError)
 	const handleCreateCard = useAloneAction(cardsSectionExternalActions.createBankCard)
 
 	const onBalanceInput = useCallback((value: string) => {
@@ -51,40 +67,63 @@ function AddAccountContent() {
 			setBalance(value)
 			return
 		}
+		if (errorsSet.has('invalidBalance')) {
+			handleRemoveError('invalidBalance')
+		}
 		const stringNumber = removeSpaces(value)
 		if (!isNumber(stringNumber)) return
 		setBalance(formatMoney(stringNumber))
-	}, [])
+	}, [errorsSet])
+
+	const onNameInput =  useCallback((value: string) => {
+		if (errorsSet.has('invalidName')) {
+			handleRemoveError('invalidName')
+		}
+		setName(value)
+	}, [errorsSet])
 
 	return (<>
-		<div className={'flex flex-col flex-grow'}>
+		<div className='flex flex-col flex-grow'>
 			<TextField
 				value={name}
-				onInput={setName}
+				onInput={onNameInput}
 				style={'simple'}
-				inputClass='bg-transparent leading-5 font-bold text-xl text-slate-900 font-sans'
+				inputClass={joinStrings(
+				'bg-transparent leading-5 font-bold text-xl font-sans',
+					errorsSet.has('invalidName') ? 'placeholder:text-red-400' : 'text-slate-900'
+				)}
 				placeholder='Tinkof'
 			/>
-			<div className='flex items-center leading-5 pl-2.5 text-slate-700'>
+			<div className={joinStrings(
+				'flex items-center leading-5 pl-2.5',
+				errorsSet.has('invalidBalance') ? 'text-red-400' : 'text-slate-700',
+			)}>
 				<span className='mr-1'>Balance:</span>
 				<TextField
 					value={balance}
 					onInput={onBalanceInput}
 					style='simple'
-					inputClass='bg-transparent leading-5 text-slate-900 font-sans'
+					inputClass={joinStrings(
+						'bg-transparent leading-5 font-sans',
+						errorsSet.has('invalidBalance') ? 'placeholder:text-red-400' : 'text-slate-700',
+					)}
 					placeholder='0'
 				/>
 			</div>
 		</div>
 		<CheckCircleIcon
 			className='w-6 h-6 ml-2 shrink-0 text-green-400 transition hover:text-green-600'
-			onClick={() => handleCreateCard()}
+			onClick={() => handleCreateCard({
+				name,
+				balance,
+			})}
 		/>
 		<XCircleIcon
 			className='w-6 h-6 ml-2 shrink-0 text-red-400 transition hover:text-red-600'
 			onClick={event => {
 				event.stopPropagation()
 				handleSetNormal()
+				handleClearErrors()
 			}}
 		/>
 	</>)
