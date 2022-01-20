6a1fccb9f65ed882ab1b2d2c89204041a0205be0
yakov.algaev <bezvrednyy@gmail.com>
2022-01-20 12:05:24 +0300

    Перевёл состояния карточки обратно в компонент. Стилизовал карточку

--- pages/main-space/cardsSection/view/AddAccountButton.tsx
+++ pages/main-space/cardsSection/view/AddAccountButton.tsx
@@ -1,9 +1,9 @@
-import {joinStrings} from '../../../../common/utils/string'
+import {isNumber} from '../../../../common/utils/number'
+import {formatMoney} from '../../../../common/utils/productUtils'
+import {joinStrings, removeSpaces} from '../../../../common/utils/string'
 import {PlusIcon} from '@heroicons/react/solid'
-import {useRef} from 'react'
+import {useCallback, useState} from 'react'
 import {useAloneAction} from '../../../../commonClient/declareAloneAction'
-import {getCurrencySymbolById, userSettingsAtom} from '../../../../commonClient/environment/userSettingsAtom'
-import {useEventHandler} from '../../../../commonClient/hooks/useEventHandler'
 import {TextField} from '../../../../commonClient/uikit/textField/TextField'
 import {CheckCircleIcon, XCircleIcon} from '@heroicons/react/outline'
 import {useAction, useAtom} from '@reatom/react'
@@ -13,7 +13,6 @@ import {cardsSectionExternalActions} from '../model/externalActions'
 function AddAccountButton() {
 	const [status] = useAtom(addAccountButtonAtoms.statusesAtom)
 	const handleSetOpened = useAction(addAccountButtonAtoms.statusesAtom.setOpened)
-	const handleCreateCard = useAloneAction(cardsSectionExternalActions.createBankCard)
 
 	function getContent(): JSX.Element {
 		if (status === 'normal') {
@@ -26,19 +25,10 @@ function AddAccountButton() {
 		return <AddAccountContent/>
 	}
 
-	const ref = useRef<HTMLDivElement>(null)
-	useEventHandler(ref, 'keydown', event => {
-		if (event.code === 'Enter') {
-			handleCreateCard()
-			console.log('save bank card')
-		}
-	})
-
 	return (
 		<div
-			ref={ref}
 			className={joinStrings(
-				'group flex box-border w-full px-8 py-3 rounded-full items-center justify-center',
+				'group flex box-border w-full px-8 h-20 rounded-full items-center justify-center',
 				'cursor-pointer hover:bg-white hover:shadow-xl transition duration-300',
 				status === 'normal' ? 'bg-slate-50 shadow-lg' : 'bg-white shadow-xl'
 			)}
@@ -50,39 +40,40 @@ function AddAccountButton() {
 }
 
 function AddAccountContent() {
-	const [name] = useAtom(addAccountButtonAtoms.nameAtom)
-	const [balance] = useAtom(addAccountButtonAtoms.balanceAtom)
-	const [userSettings] = useAtom(userSettingsAtom)
-	const currencySymbol = getCurrencySymbolById(userSettings.currencyId)
+	const [name, setName] = useState('')
+	const [balance, setBalance] = useState('')
 	const handleSetNormal = useAction(addAccountButtonAtoms.statusesAtom.setNormal)
-	const handleSetName = useAction(addAccountButtonAtoms.nameAtom.set)
-	const handleSetBalance = useAction(addAccountButtonAtoms.balanceAtom.set)
+
 	const handleCreateCard = useAloneAction(cardsSectionExternalActions.createBankCard)
+
+	const onBalanceInput = useCallback((value: string) => {
+		if (value === '') {
+			setBalance(value)
+			return
+		}
+		const stringNumber = removeSpaces(value)
+		if (!isNumber(stringNumber)) return
+		setBalance(formatMoney(stringNumber))
+	}, [])
 
 	return (<>
 		<div className={'flex flex-col flex-grow'}>
 			<TextField
 				value={name}
-				onInput={handleSetName}
+				onInput={setName}
 				style={'simple'}
-				inputClass={joinStrings(
-					'bg-transparent leading-5 font-bold text-xl text-slate-900 font-sans',
-					'group-hover:text-white',
-				)}
+				inputClass='bg-transparent leading-5 font-bold text-xl text-slate-900 font-sans'
+				placeholder='Tinkof'
 			/>
-			<div className='leading-5 pl-2.5 text-slate-700'>
-				<span>Balance:</span>
+			<div className='flex items-center leading-5 pl-2.5 text-slate-700'>
+				<span className='mr-1'>Balance:</span>
 				<TextField
 					value={balance}
-					onInput={handleSetBalance}
+					onInput={onBalanceInput}
 					style='simple'
-					inputType='number'
-					inputClass={joinStrings(
-						'bg-transparent leading-5 font-bold text-xl text-slate-900 font-sans',
-						'group-hover:text-white',
-					)}
+					inputClass='bg-transparent leading-5 text-slate-900 font-sans'
+					placeholder='0'
 				/>
-				<span>{currencySymbol}</span>
 			</div>
 		</div>
 		<CheckCircleIcon
@@ -91,7 +82,10 @@ function AddAccountContent() {
 		/>
 		<XCircleIcon
 			className='w-6 h-6 ml-2 shrink-0 text-red-400 transition hover:text-red-600'
-			onClick={handleSetNormal}
+			onClick={event => {
+				event.stopPropagation()
+				handleSetNormal()
+			}}
 		/>
 	</>)
 }
